70c5dd822eaf608eb57b24203749489acbba0421
deepak jain <deepakjain0621@gmail.com>
2025-02-09 19:25:32 +0530

    Second Pattern Did

--- First.cpp
+++ First.cpp
@@ -9,12 +9,18 @@ void pattern1(){
         cout << endl;
     }
 }
-
-
+void pattern2(int t){
+    for (int i = 0; i < t; i++){
+        for (int j = 0; j < t; j++){
+            cout << " D ";
+        }
+        cout << endl;
+    }
+}
 
 int main(){
-    // int t;
-    // cin >> t;
-    pattern1();
+    int t;
+    cin >> t;
+    pattern2(t);
     return 0;
 }
