2af7b22e7f3d8c0aa1ac90ab85b5a139bf00dbed
deepak jain <deepakjain0621@gmail.com>
2025-02-09 16:40:53 +0530

    Simple Pattern Added

--- First.cpp
+++ First.cpp
@@ -2,9 +2,17 @@
 using namespace std;
 
 void pattern1(){
-    for()
+    for(int i=0; i<10; i++){
+        for(int j=0; j<6; j++){
+            cout << "💖";
+        }
+        cout << endl;
+    }
 }
 
 int main(){
+    int t;
+    cin >> t;
+    pattern1();
     return 0;
 }
