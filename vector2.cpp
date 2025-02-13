2756f5afcc2ce5681e2b62dfb2162d2335e31369
deepak jain <deepakjain0621@gmail.com>
2025-02-13 17:50:10 +0530

    added file Vector2.cpp

---
+++ vector2.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int> v;
+    v.push_back(1);
+    cout << v.at(0) << endl;
+    pair<int, int> g(1, 4);
+    cout << g.first << " " << g.second << endl;
+
+
+    return 0;
+}
