a95cb0699a8a348926cfc73573b717d5392545c1
deepak jain <deepakjain0621@gmail.com>
2025-02-16 22:58:09 +0530

    tjs

--- for.cpp
+++ for.cpp
@@ -3,6 +3,10 @@ using namespace std;
 int main(){
     for (int i = 0; i < 100; i++){
         cout << i << endl;
+
+        for (int j = 1; j < i; j++){
+            cout << i<< endl;
+        }
     }
 
         return 0;
