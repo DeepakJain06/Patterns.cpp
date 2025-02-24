20518902035bbca2a9f4a2a3dffc714451e86dfd
deepak jain <deepakjain0621@gmail.com>
2025-02-24 22:05:53 +0530

    tis

---
+++ Recursions2.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int f(i,10){
+    if(i>n){
+        return;
+        cout << "deepak jain";
+    }
+    
+    f(i + 1, n);
+}
+
+
+int main(){
+    int n=10;
+   
+    f(1, n);
+    rename();
+
+    return 0;
+}
