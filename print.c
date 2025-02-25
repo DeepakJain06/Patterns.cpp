d98d8c60804f3bc8f490e77a4467f59c061c42b3
deepak jain <deepakjain0621@gmail.com>
2025-02-25 22:28:25 +0530

    various prog lang

---
+++ print.c
@@ -0,0 +1,7 @@
+// writing c code to print a string
+
+#include <stdio.h>
+int main(){
+    printf("Hello World\n");
+    return 0;
+}
---
+++ print.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int name = 4;
+    cout<< name <<endl ;
+    return 0;
+}
